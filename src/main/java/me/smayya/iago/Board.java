c01abffbb14071a50c34e3905762f68ea87881ab
Shreyas Mayya <smayya337@gmail.com>
2022-10-07 17:47:48 -0400

    add board defaults, more constructors, createEmptyBoard() helper

--- src/main/java/me/smayya/iago/Board.java
+++ src/main/java/me/smayya/iago/Board.java
@@ -1,12 +1,27 @@
 package me.smayya.iago;
 
 public class Board {
+    private static final String EMPTY_CHARACTER = ".";
+    private static final int DEFAULT_ROWS = 8;
+    private static final int DEFAULT_COLUMNS = 8;
     private final int rows;
     private final int columns;
+    private final int size;
+    private final String board;
 
-    public Board(int rows, int columns) {
+    public Board(int rows, int columns, String board) {
         this.rows = rows;
         this.columns = columns;
+        this.size = rows * columns;
+        this.board = board;
+    }
+
+    public Board(int rows, int columns) {
+        this(rows, columns, createEmptyBoard(rows, columns));
+    }
+
+    public Board() {
+        this(DEFAULT_ROWS, DEFAULT_COLUMNS);
     }
 
     public int getRows() {
@@ -15,5 +30,22 @@ public class Board {
 
     public int getColumns() {
         return columns;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+    private static String createEmptyBoard(int rows, int columns) {
+        int size = rows * columns;
+        String board = "";
+        for (int i = 0; i < size; i++) {
+            board += EMPTY_CHARACTER;
+        }
+        return board;
     }
 }
