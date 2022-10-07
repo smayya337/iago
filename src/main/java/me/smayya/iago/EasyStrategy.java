634e1544130350b721ca305082a2841b2d6f1432
Shreyas Mayya <smayya337@gmail.com>
2022-10-07 17:53:33 -0400

    add getMove() stub

--- src/main/java/me/smayya/iago/EasyStrategy.java
+++ src/main/java/me/smayya/iago/EasyStrategy.java
@@ -1,4 +1,8 @@
 package me.smayya.iago;
 
 public class EasyStrategy extends Strategy {
+    @Override
+    public Coordinate getMove(Board board, Player player) {
+        return new Coordinate(0, 0);
+    }
 }
