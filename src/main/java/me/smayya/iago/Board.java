5f18c2fcc593caea7fd5174763fff94c10d55752
Shreyas Mayya <smayya337@gmail.com>
2022-10-07 17:32:24 -0400

    fill in Board class with rows and columns fields

--- src/main/java/me/smayya/iago/Board.java
+++ src/main/java/me/smayya/iago/Board.java
@@ -1,4 +1,19 @@
 package me.smayya.iago;
 
 public class Board {
+    private final int rows;
+    private final int columns;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
 }
