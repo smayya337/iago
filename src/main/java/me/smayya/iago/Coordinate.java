e3089911d9baf1db0fd6eefe797b5f19ce3a749b
Shreyas Mayya <smayya337@gmail.com>
2022-10-08 11:19:28 -0400

    add index/coordinate converters and equals() method for Coordinate

--- src/main/java/me/smayya/iago/Coordinate.java
+++ src/main/java/me/smayya/iago/Coordinate.java
@@ -16,4 +16,19 @@ public class Coordinate {
     public int getColumn() {
         return column;
     }
+
+    public static int getIndexFromCoordinate(Coordinate coordinate, int rows) {
+        int startOfRow = coordinate.getRow() * rows;
+        return startOfRow + coordinate.getColumn();
+    }
+
+    public static Coordinate getCoordinateFromIndex(int index, int rows) {
+        int rowNumber = index / rows;
+        int columnNumber = index % rows;
+        return new Coordinate(rowNumber, columnNumber);
+    }
+
+    public boolean equals(Coordinate obj) {
+        return this.row == obj.getRow() && this.column == obj.getColumn();
+    }
 }
