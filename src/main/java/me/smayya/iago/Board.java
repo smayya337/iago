148b18d06c9361014d315f681da12739d677211c
Shreyas Mayya <smayya337@gmail.com>
2022-10-09 18:54:46 -0400

    force the board to be a square

--- src/main/java/me/smayya/iago/Board.java
+++ src/main/java/me/smayya/iago/Board.java
@@ -5,41 +5,34 @@ import java.util.stream.Collectors;
 
 public class Board {
     private static final String EMPTY_CHARACTER = ".";
-    private static final int DEFAULT_ROWS = 8;
-    private static final int DEFAULT_COLUMNS = 8;
+    private static final int DEFAULT_SIDE_LENGTH = 8;
     private static final String[] DIRECTIONS = {"up", "down", "left", "right", "upleft", "upright", "downleft", "downright"};
-    private final int rows;
-    private final int columns;
+    private final int sideLength;
     private final int size;
     private String board;
     private final Map<String, Integer> counts;
     private final Set<Coordinate> coordinates;
     private final Map<String, List<Coordinate>[]> relationships;
 
-    public Board(int rows, int columns, String board) {
-        this.rows = rows;
-        this.columns = columns;
-        this.size = rows * columns;
+    public Board(int sideLength, String board) {
+        this.sideLength = sideLength;
+        this.size = sideLength * sideLength;
         this.board = board;
         this.counts = initializeCounts(board);
-        this.coordinates = initializeCoordinates(rows, columns);
+        this.coordinates = initializeCoordinates(sideLength);
         this.relationships = initializeRelationships(coordinates);
     }
 
-    public Board(int rows, int columns) {
-        this(rows, columns, createEmptyBoard(rows, columns));
+    public Board(int sideLength) {
+        this(sideLength, createEmptyBoard(sideLength));
     }
 
     public Board() {
-        this(DEFAULT_ROWS, DEFAULT_COLUMNS);
+        this(DEFAULT_SIDE_LENGTH);
     }
 
-    public int getRows() {
-        return rows;
-    }
-
-    public int getColumns() {
-        return columns;
+    public int getSideLength() {
+        return sideLength;
     }
 
     public int getSize() {
@@ -50,8 +43,8 @@ public class Board {
         return board;
     }
 
-    private static String createEmptyBoard(int rows, int columns) {
-        int size = rows * columns;
+    private static String createEmptyBoard(int sideLength) {
+        int size = sideLength * sideLength;
         String board = "";
         for (int i = 0; i < size; i++) {
             board += EMPTY_CHARACTER;
@@ -64,7 +57,7 @@ public class Board {
     }
 
     private boolean isEmpty(Coordinate coordinate) {
-        int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+        int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
         String value = String.valueOf(board.charAt(index));
         return value.equals(EMPTY_CHARACTER);
     }
@@ -78,7 +71,7 @@ public class Board {
     }
 
     private Set<Coordinate> flippedSpots(Coordinate coordinate, Player player) {
-        int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+        int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
         Set<Coordinate> spots = new HashSet<>();
         for (String direction :
                 relationships.keySet()) {
@@ -120,7 +113,7 @@ public class Board {
     }
 
     private void flip(Coordinate coordinate, Player player) {
-        int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+        int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
         String originalToken = String.valueOf(board.charAt(index));
         replaceBoard(index, player);
         updateCounts(originalToken, -1);
@@ -145,10 +138,10 @@ public class Board {
         return counts;
     }
 
-    private static Set<Coordinate> initializeCoordinates(int rows, int columns) {
+    private static Set<Coordinate> initializeCoordinates(int sideLength) {
         Set<Coordinate> coordinates = new HashSet<>();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
                 coordinates.add(new Coordinate(i, j));
             }
         }
@@ -158,102 +151,102 @@ public class Board {
     private Map<String, List<Coordinate>[]> initializeRelationships(Set<Coordinate> coordinates) {
         Map<String, List<Coordinate>[]> relationships = new HashMap<>();
         ArrayList<List<Coordinate>[]> relationsArrayList = new ArrayList<>();
-        relationsArrayList.add(initializeUps(coordinates, rows));
-        relationsArrayList.add(initializeDowns(coordinates, rows));
-        relationsArrayList.add(initializeLefts(coordinates, rows));
-        relationsArrayList.add(initializeRights(coordinates, rows));
-        relationsArrayList.add(initializeUpLefts(coordinates, rows));
-        relationsArrayList.add(initializeUpRights(coordinates, rows));
-        relationsArrayList.add(initializeDownLefts(coordinates, rows));
-        relationsArrayList.add(initializeDownRights(coordinates, rows));
+        relationsArrayList.add(initializeUps(coordinates, sideLength));
+        relationsArrayList.add(initializeDowns(coordinates, sideLength));
+        relationsArrayList.add(initializeLefts(coordinates, sideLength));
+        relationsArrayList.add(initializeRights(coordinates, sideLength));
+        relationsArrayList.add(initializeUpLefts(coordinates, sideLength));
+        relationsArrayList.add(initializeUpRights(coordinates, sideLength));
+        relationsArrayList.add(initializeDownLefts(coordinates, sideLength));
+        relationsArrayList.add(initializeDownRights(coordinates, sideLength));
         for (int i = 0; i < relationsArrayList.size(); i++) {
             relationships.put(DIRECTIONS[i], relationsArrayList.get(i));
         }
         return relationships;
     }
 
-    private static List<Coordinate>[] initializeUps(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeUps(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isUp).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeDowns(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeDowns(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isDown).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeLefts(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeLefts(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isLeft).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeRights(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeRights(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isRight).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeUpLefts(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeUpLefts(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isUpLeft).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeUpRights(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeUpRights(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isUpRight).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeDownLefts(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeDownLefts(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isDownLeft).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
-    private static List<Coordinate>[] initializeDownRights(Set<Coordinate> coordinates, int rows) {
+    private static List<Coordinate>[] initializeDownRights(Set<Coordinate> coordinates, int sideLength) {
         List<Coordinate>[] relations = new List[coordinates.size()];
         for (Coordinate coordinate :
                 coordinates) {
-            int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+            int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
             relations[index] = coordinates.stream().filter(coordinate::isDownRight).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
         }
         return relations;
     }
 
     public String getTokenAtCoordinate(Coordinate coordinate) {
-        int index = Coordinate.getIndexFromCoordinate(coordinate, rows);
+        int index = Coordinate.getIndexFromCoordinate(coordinate, sideLength);
         return String.valueOf(board.charAt(index));
     }
 }
