32e32573f0049a687e676a73a5ac1051c06279b1
Shreyas Mayya <smayya337@gmail.com>
2022-10-09 21:00:42 -0400

    add basic code for Game

--- src/main/java/me/smayya/iago/Game.java
+++ src/main/java/me/smayya/iago/Game.java
@@ -1,4 +1,43 @@
 package me.smayya.iago;
 
+import java.util.Arrays;
+
 public class Game {
+    private Board board;
+    public Game(Board board) {
+        this.board = board;
+    }
+
+    public Game() {
+        this(new Board());
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public boolean isOver() {
+        int occupiedSpaces = 0;
+        for (Player player:
+             Player.values()) {
+            int playerSpaces = board.getCount(player);
+            if (playerSpaces == 0) {
+                return true;
+            }
+            occupiedSpaces += playerSpaces;
+        }
+        return occupiedSpaces == board.getSize();
+    }
+
+    public Player getWinner() {
+        if (!isOver()) return null;
+        return Arrays.stream(Player.values()).max((x1, x2) -> board.getCount(x1) - board.getCount(x2)).orElse(null);
+    }
+
+    public void move(Coordinate coordinate, Player player) {
+        if (isOver()) {
+            throw new RuntimeException("Game is already over!");
+        }
+        board.move(coordinate, player);
+    }
 }
