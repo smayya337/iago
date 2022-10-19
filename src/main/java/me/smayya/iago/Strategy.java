a558d699ed4eb0e9e983c21c73688ad5701e9b98
Shreyas Mayya <smayya337@gmail.com>
2022-10-19 12:12:46 -0400

    add equals() and hashCode() for all Strategy instances

--- src/main/java/me/smayya/iago/Strategy.java
+++ src/main/java/me/smayya/iago/Strategy.java
@@ -2,4 +2,14 @@ package me.smayya.iago;
 
 public abstract class Strategy {
     public abstract Coordinate getMove(Board board, Player player);
+
+    @Override
+    public boolean equals(Object obj) {
+        return getClass().equals(obj.getClass());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
