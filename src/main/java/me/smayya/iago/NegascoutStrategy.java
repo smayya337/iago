d21985cda7a7e9aedf515fbed2b261065110a8df
Shreyas Mayya <smayya337@gmail.com>
2022-10-22 12:08:02 -0400

    add stub for negascout strategy

---
+++ src/main/java/me/smayya/iago/NegascoutStrategy.java
@@ -0,0 +1,8 @@
+package me.smayya.iago;
+
+public class NegascoutStrategy extends Strategy {
+    @Override
+    public Coordinate getMove(Board board, Player player) {
+        return null;
+    }
+}
